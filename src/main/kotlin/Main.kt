3b349a7893ce2c0866db5ce7f86a673d025fadc6
ivan [itb] martinez canyero <ivan.martinez.7e6@itb.cat>
2023-02-09 12:55:22 +0100

    problemas2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,6 @@
 import java.io.File
-import java.io.BufferedReader
-import java.io.FileReader
-import java.nio.file.Files
-import java.nio.file.Path
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 import java.util.*
-import kotlin.io.path.*
+
 const val reset = "\u001b[0m"
 const val box = "\u001b[51m"
 const val bold = "\u001b[1m"
@@ -26,6 +20,7 @@ const val blue = "\u001b[38;5;69m"
 
 val scanner = Scanner(System.`in`)
 fun main() {
+    val problemasResueltos = mutableListOf<ProblemaResuelto>()
     var numProblema = 0
     do {
         println("Problema $box$bold$pink ${numProblema + 1} $reset")
@@ -34,15 +29,30 @@ fun main() {
         println("Vols intentar aquest problema?\n$green$bold$box SI $reset $red$bold$box NO $reset")
         val intentar = scanner.nextLine().uppercase()
         if (intentar == "SI"){
-            if (intentarProblema(currentProblema)){
-                println("Has acertat")
+            val problema = intentarProblema(currentProblema, problemasResueltos)
+            if (problema.first){
+                val intents= problema.second
+                println("Has acertat amb $intents intents")
             } else println("No has pogut amb el problema")
             numProblema++
         } else if (intentar == "NO") numProblema++
-    } while (numProblema != 5)
+    } while (numProblema != ListaProblemas().listaProblemas.size)
 
+    println("Vols veure l'historial?\n$green$bold$box SI $reset $red$bold$box NO $reset")
+    val historial = scanner.nextLine().uppercase()
+    if (historial == "SI"){
+        for (i in 0 .. problemasResueltos.size){
+            println(problemasResueltos[i].enunciado)
+            println("$purple${bold}Entrada:$reset ${problemasResueltos[i].inputPriv}")
+            println("$purple${bold}Sortida:$reset ${problemasResueltos[i].outputPriv}")
+            println("$purple${bold}Resolt:$reset ${problemasResueltos[i].resuelto}")
+            println("$purple${bold}Intents:$reset ${problemasResueltos[i].intentos}")
+        }
+    }
 }
 
+fun printHistorial(problema: Int){
+}
 fun enunciados(currentProblema: Problema){
     var listaEnunciados = File("src/main/kotlin/enunciados/enunciados.txt").readText()
     var listaOutputPub = File("src/main/kotlin/provesPublic/outputsPublic.txt").readText()
@@ -50,37 +60,42 @@ fun enunciados(currentProblema: Problema){
 
     var listaInputPriv = File("src/main/kotlin/provesPriv/inputPriv.txt").readText()
 
-
-
-
     println(currentProblema.enunciado)
-    println("${bold}Exemple 1$reset")
+    println("$cyan${bold}Exemple 1$reset")
     println("${currentProblema.listaInputPub[0]}\n$purple${bold}Entrada:$reset ${currentProblema.listaInputPub[1]}")
     println("${currentProblema.listaOutputPub[0]}\n$purple${bold}Sortida:$reset ${currentProblema.listaOutputPub[1]}")
-    println("${bold}Exemple 2$reset")
+    println("$cyan${bold}Exemple 2$reset")
     println("$purple${bold}Entrada:$reset ${currentProblema.listaInputPub[2]}")
     println("$purple${bold}Sortida:$reset ${currentProblema.listaOutputPub[2]}")
 
 }
-fun intentarProblema(currentProblema: Problema): Int{
-    println("$purple${bold}Entrada:$reset ${currentProblema.inputPriv}")
+fun intentarProblema(currentProblema: Problema, problemasResueltos: MutableList<ProblemaResuelto>): Pair<Boolean, Int> {
+    val random = (0..2).random()
+    println("$purple${bold}Entrada:$reset ${currentProblema.inputPriv[random]}")
     println("$purple${bold}Sortida:$reset ???")
     println("Per abandonar el problema entra SORTIR")
     var userAnswer: String
+    var resolt = false
     var intents = 0
     do {
         intents++
         userAnswer = scanner.nextLine().uppercase().toString()
-        println(userAnswer)
-        if (userAnswer != currentProblema.outputPriv){
+        if (userAnswer != currentProblema.outputPriv[random].uppercase() && userAnswer != "SORTIR" ) {
             println("Resposta incorrecta, torna-ho a intentar")
+        } else if (userAnswer == currentProblema.outputPriv[random].uppercase()){
+            resolt = true
         }
-    } while (userAnswer != currentProblema.outputPriv && userAnswer != "SORTIR")
+    } while (userAnswer != currentProblema.outputPriv[random].uppercase() && userAnswer != "SORTIR")
 
     if (userAnswer == "SORTIR") intents = 0
 
-   // return userAnswer == currentProblema.outputPriv
-    return intents
+    problemasResueltos.add(ProblemaResuelto(currentProblema.enunciado,
+        currentProblema.inputPriv[random], currentProblema.outputPriv[random],resolt, intents ))
+
+    return (userAnswer == currentProblema.outputPriv[random]) to intents
+
+}
+fun historialProblemas(currentProblema: Problema, resuelto: Boolean, intentos: Int){
 
 }
 
--- src/main/kotlin/Problema.kt
+++ src/main/kotlin/Problema.kt
@@ -1,14 +1,29 @@
 import java.io.File
 
 
-open class ListaProblemas (){
+class ListaProblemas{
+    val listaProblemasResueltos = mutableListOf<ProblemaResuelto>()
+
     val listaProblemas = arrayOf(
-        Problema("Escriu un programa que donat dos nombres enters imprimeixi la seqüència entre el primer i el segon."
-        , arrayOf("Per l’entrada rebreu dos nombres enters","3 14","14 3"), arrayOf("La sortida ha de ser una seqüència de números del primer fins al segon nombre introduït, imprimint-los tot en una mateixa línia.","3,4,5,6,7,8,9,10,11,12,13,14","14,13,12,11,10,9,8,7,6,5,4,3"),
-        "4 19", "4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19"))
+        Problema("L'usuari escriu 4 enters i s'imprimeix el valor de sumar el primer amb el segon, multiplicat per el mòdul del tercer amb el quart."
+            , arrayOf("Per l’entrada rebreu 4 nombres enters introduïts per l’usuari.","2 5 10 4","23 31 156 5"), arrayOf("La sortida ha de ser un enter que representa el resultat de la següent operació (a + b) * (c % d)..","14","54"),
+            arrayOf("25 45 56 12", "23 258 12 25", "-5 2 -78 32"), arrayOf("560","3372", "42" )),
+        Problema("En una escola tenim tres classes i volem saber quin és el nombre de taules que necessitarem tenir en total.\nDependrà del nombre d'alumnes per aula. Cal tenir en compte que a cada taula hi caben 2 alumnes."
+            , arrayOf("Per l’entrada rebreu 3 enters que representen el nombre d’alumnes que hi ha a cada classe.","22 25 26","31 29 19"), arrayOf("La sortida ha de ser un enter que representi el nombre de pupitres necessaris.","37","41"),
+            arrayOf("25 11 29", "1 2 5", "25 2 1"), arrayOf("34", "5", "15")),
+        Problema("Escriu un programa que donat un nombre enter, indiqui si aquest és primer o no."
+            , arrayOf("Per l’entrada rebreu un nombre enter.","11","128"), arrayOf("La sortida ha d’imprimir si el nombre és primer o no.","Primer","No primer"),
+            arrayOf("425", "367", "666"), arrayOf("NO PRIMER", "PRIMER", "NO PRIMER")))
+
+
 }
 class Problema (var enunciado: String, var listaInputPub: Array<String>,
-                var listaOutputPub: Array<String>, var inputPriv: String, var outputPriv: String) {
+                var listaOutputPub: Array<String>, var inputPriv: Array<String>, var outputPriv: Array<String>,
+                ) {
+//, false, 0
+}
+class ProblemaResuelto (var enunciado: String, var inputPriv: String, var outputPriv: String,
+                var resuelto: Boolean, var intentos: Int) {
 
 }
 

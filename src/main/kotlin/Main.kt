dc3f0d68265cadf312ad25c90c9b5a7b1cc73072
ivan [itb] martinez canyero <ivan.martinez.7e6@itb.cat>
2023-02-09 10:46:25 +0100

    problemas

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,14 +7,43 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.*
 import kotlin.io.path.*
+const val reset = "\u001b[0m"
+const val box = "\u001b[51m"
+const val bold = "\u001b[1m"
+const val underline = "\u001b[21m"
+const val bgGold = "\u001b[43m"
+const val bgGreen = "\u001b[48;5;28m"
+const val bgGray = "\u001b[47m"
+const val red = "\u001b[31m"
+const val cyan = "\u001b[38;5;87m"
+const val green = "\u001b[38;5;10m"
+const val gold = "\u001b[33m"
+const val yellow = "\u001b[38;5;11m"
+const val gray = "\u001b[38;5;7m"
+const val pink = "\u001b[38;5;207m"
+const val purple = "\u001b[38;5;99m"
+const val blue = "\u001b[38;5;69m"
 
-
+val scanner = Scanner(System.`in`)
 fun main() {
+    var numProblema = 0
+    do {
+        println("Problema $box$bold$pink ${numProblema + 1} $reset")
+        val currentProblema = ListaProblemas().listaProblemas[numProblema]
+        enunciados(currentProblema)
+        println("Vols intentar aquest problema?\n$green$bold$box SI $reset $red$bold$box NO $reset")
+        val intentar = scanner.nextLine().uppercase()
+        if (intentar == "SI"){
+            if (intentarProblema(currentProblema)){
+                println("Has acertat")
+            } else println("No has pogut amb el problema")
+            numProblema++
+        } else if (intentar == "NO") numProblema++
+    } while (numProblema != 5)
 
-    enunciados()
 }
 
-fun enunciados(){
+fun enunciados(currentProblema: Problema){
     var listaEnunciados = File("src/main/kotlin/enunciados/enunciados.txt").readText()
     var listaOutputPub = File("src/main/kotlin/provesPublic/outputsPublic.txt").readText()
     var listaInputPub = File("src/main/kotlin/provesPublic/inputsPublic.txt").readText()
@@ -23,8 +52,48 @@ fun enunciados(){
 
 
 
-    var numProblema = 0
+
+    println(currentProblema.enunciado)
+    println("${bold}Exemple 1$reset")
+    println("${currentProblema.listaInputPub[0]}\n$purple${bold}Entrada:$reset ${currentProblema.listaInputPub[1]}")
+    println("${currentProblema.listaOutputPub[0]}\n$purple${bold}Sortida:$reset ${currentProblema.listaOutputPub[1]}")
+    println("${bold}Exemple 2$reset")
+    println("$purple${bold}Entrada:$reset ${currentProblema.listaInputPub[2]}")
+    println("$purple${bold}Sortida:$reset ${currentProblema.listaOutputPub[2]}")
+
+}
+fun intentarProblema(currentProblema: Problema): Int{
+    println("$purple${bold}Entrada:$reset ${currentProblema.inputPriv}")
+    println("$purple${bold}Sortida:$reset ???")
+    println("Per abandonar el problema entra SORTIR")
+    var userAnswer: String
+    var intents = 0
+    do {
+        intents++
+        userAnswer = scanner.nextLine().uppercase().toString()
+        println(userAnswer)
+        if (userAnswer != currentProblema.outputPriv){
+            println("Resposta incorrecta, torna-ho a intentar")
+        }
+    } while (userAnswer != currentProblema.outputPriv && userAnswer != "SORTIR")
+
+    if (userAnswer == "SORTIR") intents = 0
+
+   // return userAnswer == currentProblema.outputPriv
+    return intents
+
+}
+
+/*
+var listaEnunciados = File("src/main/kotlin/enunciados/enunciados.txt").readText()
+    var listaOutputPub = File("src/main/kotlin/provesPublic/outputsPublic.txt").readText()
+    var listaInputPub = File("src/main/kotlin/provesPublic/inputsPublic.txt").readText()
+
+    var listaInputPriv = File("src/main/kotlin/provesPriv/inputPriv.txt").readText()
+
+
+
     var currentProblema = Problema(numProblema, listaEnunciados, listaInputPub, listaOutputPub,listaInputPriv)
     println(currentProblema.enunciado)
     println(currentProblema.printProblemaPublic())
-}
+ */
--- src/main/kotlin/Problema.kt
+++ src/main/kotlin/Problema.kt
@@ -1,5 +1,18 @@
 import java.io.File
 
+
+open class ListaProblemas (){
+    val listaProblemas = arrayOf(
+        Problema("Escriu un programa que donat dos nombres enters imprimeixi la seqüència entre el primer i el segon."
+        , arrayOf("Per l’entrada rebreu dos nombres enters","3 14","14 3"), arrayOf("La sortida ha de ser una seqüència de números del primer fins al segon nombre introduït, imprimint-los tot en una mateixa línia.","3,4,5,6,7,8,9,10,11,12,13,14","14,13,12,11,10,9,8,7,6,5,4,3"),
+        "4 19", "4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19"))
+}
+class Problema (var enunciado: String, var listaInputPub: Array<String>,
+                var listaOutputPub: Array<String>, var inputPriv: String, var outputPriv: String) {
+
+}
+
+/*
 class Problema (var numProblema: Int, var listaEnunciados: String, var listaInputPub: String,
                 var listaOutputPub: String, var listaInputPriv: String) {
 
@@ -16,3 +29,4 @@ class Problema (var numProblema: Int, var listaEnunciados: String, var listaInpu
     }
 
 }
+ */
